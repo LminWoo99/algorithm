de1d67c3caf36270fcfc855682e29dbf25a7e14d
LminWoo99 <mw310@naver.com>
2023-02-13 16:45:36 +0900

    0213 commit

---
+++ 섹션4/섹션6/바둑이승차.py
@@ -0,0 +1,23 @@
+import sys
+def DFS(L, sum, tsum):
+    global result
+    if sum+(total-tsum)<result:
+        return
+    if sum>c:
+        return
+    if L==n:
+        if sum>result:
+            result=sum
+    else:
+        DFS(L+1, sum+a[L], tsum+a[L])
+        DFS(L+1, sum, tsum+a[L])
+
+
+c, n=map(int, input().split())
+a=[0]*n
+result=-2147000000
+for i in range(n):
+    a[i]=int(input())
+total=sum(a)
+DFS(0, 0, 0)
+print(result)
