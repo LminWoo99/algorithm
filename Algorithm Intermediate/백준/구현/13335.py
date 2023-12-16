93a12041370692257f954f93ae9e549809fd6c11
LminWoo99 <mw310@naver.com>
2023-12-16 23:41:29 +0900

    1216 -구현

--- Algorithm Intermediate/백준/구현/13335.py
+++ Algorithm Intermediate/백준/구현/13335.py
@@ -1,38 +1,17 @@
 import sys
 input=sys.stdin.readline
 from collections import deque
+
 n, w,l=map(int, input().split())
 truck=list(map(int, input().split()))
-i=0
-index=0
-res=0
-cur_weight=0
-dq=deque()
-dq.append(truck[0])
-index+=1
-time=w
-while dq:
-    i+=1
-    time-=1
-    if time==0:
-        dq.popleft()
-        time=w
-    if len(truck)>index and sum(dq)+truck[index]<=l:
-        dq.append(truck[index])
-        index+=1
-print(i)
-# while truck:
-#    if i>w:
-#         res+=i
-#         i=0
-#         cur_weight=0
-#    if len(truck)!=1 and truck[0]+cur_weight<=l:
-#         x=truck.popleft()
-#         cur_weight+=x
-#    if len(truck)==1:
-#        res+=(w+1)
-#        break
-#    i+=1
-
-
-    
+bridge=[0]*w
+time=0
+while bridge:
+    time+=1
+    bridge.pop(0)
+    if truck:
+        if sum(bridge)+truck[0]<=l:
+            bridge.append(truck.pop(0))
+        else:
+            bridge.append(0)
+print(time)
