01e4b791af4749865ae861d8b28e8cd528b9c744
LminWoo99 <mw310@naver.com>
2023-12-24 17:47:45 +0900

    1224 - 구현

--- Algorithm Intermediate/백준/구현/16236.py
+++ Algorithm Intermediate/백준/구현/16236.py
@@ -1,56 +1,68 @@
-import sys
-input=sys.stdin.readline
 from collections import deque
 
-def solution(x, y, cnt):
-    global cnt_size
-    tmp=0
-    dq=deque()
-    dq.append((x,y, cnt))
-    visit[x][y]=1
-    eat=0
-    while dq:
-        if eat==check:
-            break
-        else:
-            x,y,cnt=dq.popleft()
-            for i in range(4): 
-                xx, yy= x+dx[i], y+dy[i]
-                if 0<=xx<n and 0<=yy<n:
-                    if visit[xx][yy][cnt+1]==0 and board[xx][yy]==0:
-                        visit[xx][yy][cnt+1]=1
+N = int(input())
+space = [list(map(int, input().split())) for _ in range(N)]
 
-                    elif visit[xx][yy][cnt+1]==0 and board[xx][yy]<cnt_size:
-                        visit[xx][yy][cnt+1]=1
-                        tmp+=1
-                        if tmp==cnt_size:
-                            eat+=1
-                            tmp=0
-                    elif visit[xx][yy][cnt+1]==0 and board[xx][yy]==cnt_size:
-                        visit[xx][yy][cnt+1]=1
+dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
+
+pos = []
+for i in range(N):
+    for j in range(N):
+        if space[i][j] == 9:
+            pos.append(i)
+            pos.append(j)
+
+cnt = 0
+def bfs(x, y):
+    visited = [[0]*N for _ in range(N)]
+    queue = deque([[x,y]])
+    cand = []
+
+    visited[x][y] = 1
+
+    while queue:
+        i, j = queue.popleft()
+
+        for idx in range(4):
+            ii, jj = i + dx[idx] , j + dy[idx]
+
+            if 0 <= ii and ii < N and 0 <= jj and jj < N and visited[ii][jj] == 0:
+				# 5. 간선은 상하 좌우지만 조건에 따라서 움직이기 때문에 조건을 상세하여야한다.
+                if space[x][y] > space[ii][jj] and space[ii][jj] != 0:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    cand.append((visited[ii][jj] - 1, ii, jj))
+                elif space[x][y] == space[ii][jj]:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    queue.append([ii,jj])
+                elif space[ii][jj] == 0:
+                    visited[ii][jj] =  visited[i][j] + 1
+                    queue.append([ii,jj])
                     
-                    dq.append((xx,yy, cnt+1))
-    return cnt
-                        
-
-                    
-                
-n=int(input())
-dx=[-1,0,1,0]
-dy=[0, -1,0 ,-1]
-cnt_size=2
-board=[list(map(int, input().split())) for _ in range(n)]
-visit=[[[0]*n for _ in range(n)] for _ in range(n)]
-check=0
-for i in range(n):
-    for j in range(n):
-        if board[i][j]==9:
-            x,y =i,j
-        elif board[i][j]!=9 and board[i][j]!=0:
-            check+=1
-            
+	# 6. 후보 리스트는 우선 순위가 있기 때문에 정렬을 사용할 수 있다.
+    return sorted(cand, key = lambda x: (x[0], x[1], x[2]))
 
 
-cnt=solution(i,j,0)
+i, j = pos
+size = [2, 0]
+# 8. 맨 앞의 후보만 먹고 위치 이동후 다시 BFS 해야한다
+while True:
+    space[i][j] = size[0]
+    cand = deque(bfs(i,j))
+    
+    if not cand:
+        break
+        
+    # 7. 후보리스트가 나오면 맨 앞의 후보 먹이를 뽑아 그 위치로 이동한다.
+    step, xx, yy = cand.popleft()
+    cnt += step
+    size[1] += 1
+    
+	# 9. 몇 개를 먹었는지 몇 초간 이동했는지 체크해 줄 필요가 있다.
+    if size[0] == size[1]:
+        size[0] += 1
+        size[1] = 0
+
+    space[i][j] = 0
+    i, j = xx, yy
+        
 print(cnt)
-
---
+++ Algorithm Intermediate/백준/구현/17140.py
@@ -0,0 +1,53 @@
+import sys
+input=sys.stdin.readline
+def line(a):
+    max_tmp=0
+    for i in range(len(a)):
+        check=[0]*101
+        tmp=[]
+        for j in a[i]:
+            check[j]+=1
+        for k in range(1, 101):
+            if check[k]!=0:
+                tmp.append((k, check[k]))
+        tmp.sort(key=lambda x:(x[1], x[0]))
+        max_tmp=max(max_tmp, len(tmp))
+    
+        a[i]=tmp
+    max_tmp*=2
+    result=[]
+    for i in a:
+        new_a=[]
+        for j in i:
+            new_a.extend(j)
+        if len(new_a)<max_tmp:
+            new_a.extend([0]* (max_tmp-len(new_a)))
+        result.append(new_a)
+    return result
+
+    
+r,c,k=map(int, input().split())
+a=[list(map(int, input().split())) for _ in range(3)]
+cnt=0
+while True:
+    if len(a)>=r and len(a[0])>=c and a[r-1][c-1]==k:
+            print(cnt) 
+            break
+    elif cnt>100:
+        print(-1)
+        break
+    else:
+        cnt+=1
+        ## 행의 갯수
+
+        line_len=len(a)
+        row_len=len(a[0])
+        if line_len>=row_len:
+            a=line(a)
+        else:
+            a=list(map(list, zip(*a)))
+            a=line(a)
+            a=list(map(list, zip(*a)))
+
+
+
