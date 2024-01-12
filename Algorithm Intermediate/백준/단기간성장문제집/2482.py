52fd96a8b6b43951f3dbbc8a26f3097a07312a29
LminWoo99 <mw310@naver.com>
2024-01-12 17:03:19 +0900

    0112

---
+++ Algorithm Intermediate/백준/단기간성장문제집/2482.py
@@ -0,0 +1,27 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+k=int(input())
+dp=[[0]*(k+1) for _ in range(n+1)]
+div=1000000003
+for i in range(n+1):
+    dp[i][0]=1
+    dp[i][1]=i
+print(dp)
+
+for i in range(2, n+1):
+    for j in range(2, k+1):
+        if i==n:
+            dp[i][j]=dp[i-3][j-1]+dp[i-1][j]
+        else:
+            dp[i][j]=dp[i-2][j-1]+dp[i-1][j]
+        dp[i][j]%=div
+        
+print(dp [n][k])
+        
+        
+ 
+ 
+ 
+5 
