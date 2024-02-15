186b221ebfce7fcebb2791f5f1fc49cb4477ea8e
LminWoo99 <mw310@naver.com>
2024-02-15 15:52:36 +0900

    programmers- 이모티콘할인행사,  표현가능한 이진트리

--- 프로그래머스/표현가능한이진트리.py
+++ 프로그래머스/표현가능한이진트리.py
@@ -1,39 +1,31 @@
-# # # def solution(numbers):
-# # #     answer = []
-# # #     def check_binary(x):
-# # #         returnX=""
-# # #         while x>=1:
-# # #             tmp=str(x%2)
-# # #             returnX+=tmp
-# # #             x//=2
-# # #         return returnX
-            
-# # #     for i in range(len(numbers)):
-# # #         print(check_binary(numbers[i]))
-# # #     return answer
-# # # solution([7, 42, 5])
-# # def check_digit(x):
-# #     n=1
-# #     while True:
-# #         if 2**n-1>x:
-# #             return 0
-# #         if 2**n-1==x:
-# #             return 1
-# #         n+=1
-# # print(check_digit(31))
-# # # 0101010
-# # # 1011111
-# # # 111
-# # # 루트노드는 0이면 안됨
-# # 01010
-# # 42/2=21
-# # 21/2=10
-# # 10/2=5
-# # 5/2=2
-# # 2/2=1
-# # 1/1
-
-# print(bin(5)[2:])
-binary_num="123"
-binary_num="0"+binary_num
-print(binary_num)
+def solution(numbers):
+    answer = []
+    def dfs(b, i, depth):
+        if depth==0:
+            return True
+        if b[i]=="0" and (b[i-depth]=="1" or b[i+depth]=="1"):
+            return False
+        # 왼쪽 서브트리
+        left=dfs(b, i-depth, depth//2)
+        right=dfs(b, i+depth, depth//2)
+        return left and right
+                
+        
+    def check_digit(x, binary_num):
+        n=1
+        while True:
+            if 2**n-1>x:
+                binary_num=((2**n-1)-x)*"0"+binary_num
+                return binary_num
+            if 2**n-1==x:
+                return binary_num
+            n+=1
+    for i in range(len(numbers)):
+        if numbers[i]==0:
+            answer.append(0)
+            continue
+        binary_num=bin(numbers[i])[2:]
+        binary_num=check_digit(len(binary_num), binary_num)
+        result=dfs(binary_num, len(binary_num)//2, (len(binary_num)+1)//4)
+        answer.append(1 if result else 0)
+    return answer
