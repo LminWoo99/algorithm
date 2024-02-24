575083e5883f329364dff561747358502e38cf75
LminWoo99 <mw310@naver.com>
2024-02-24 17:59:21 +0900

    programmers- 귤고르기, 디펜스

---
+++ 프로그래머스/귤고르기.py
@@ -0,0 +1,34 @@
+# from collections import Counter
+# import heapq
+
+# def solution(k, tangerine):
+#     answer = 0
+#     a=dict(Counter(tangerine))
+#     hq=[]
+#     for x,y in a.items():
+#         heapq.heappush(hq, -y)
+#     cnt=0
+#     while hq:
+#         print(hq)
+#         y=heapq.heappop(hq)
+#         answer+=1
+#         cnt+=-y
+#         print(cnt)
+#         if cnt>=k:
+#             break
+#     return answer
+from collections import Counter
+
+
+def solution(k, tangerine):
+    answer = 0
+    a=Counter(tangerine)
+    for i in sorted(a.values(),reverse=True):
+        k-=i
+        answer+=1
+        if k<=0:
+            break
+    return answer
+    
+
+print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3]))
--- 프로그래머스/디펜스게임.py
+++ 프로그래머스/디펜스게임.py
@@ -1,11 +1,16 @@
 import heapq
-# def solution(n, k, enemy):
-#     answer = 0
-
-        
-#     return answer
-
-a=[1,2,3,4,5]
-hq=[]
-heapq.heappush(-a,hq)
-print(hq)
+def solution(n, k, enemy):
+    answer = 0
+    hq=[]
+    temp=0
+    for i in enemy:
+        heapq.heappush(hq, -i)          
+        temp+=i
+        if temp>n:
+            if k==0:
+                break
+            temp+=heapq.heappop(hq)
+            k-=1
+        answer+=1
+            
+    return answer
