7a8c90473514dc7b4b15277eb76bf837dbc0f5dc
이민우 <mw310@naver.com>
2024-05-28 15:32:42 +0900

    [Gold IV] Title: 비슷한 단어, Time: 104 ms, Memory: 33728 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.py
@@ -1,31 +1,40 @@
-n = int(input())
-a = [input() for _ in range(n)]
-b = sorted(list(enumerate(a)),key = lambda x: x[1])
-def check(a, b):
-    cnt = 0
-    for i in range(min(len(a), len(b))):
-        if a[i] == b[i]: cnt += 1
-        else: break
-    return cnt
+import sys
+input=sys.stdin.readline
 
+n=int(input())
+temp=[]
+for _ in range(n):
+    temp.append(input().rstrip())
+wow=dict()
+for idx,val in enumerate(temp):
+    wow[val]=idx
 
-length = [0] * (n+1)
-maxlength = 0
-
+a=temp[:]
+a.sort()
+res=0
+res_len=[0]*(n+1)
 for i in range(n-1):
-    tmp = check(b[i][1], b[i+1][1])
-    maxlength = max(maxlength, tmp)
-
-    length[b[i][0]] = max(length[b[i][0]], tmp)
-    length[b[i+1][0]] = max(length[b[i+1][0]], tmp)
-first = 0
+    x,y=a[i], a[i+1]
+    tmp=0
+    j=0
+    while j<len(x) and j<len(y):
+        if x[j]==y[j]:
+            tmp+=1
+            j+=1
+        else:
+            break
+    res=max(res, tmp)
+    res_len[wow[x]]=max(res_len[wow[x]], tmp)
+    res_len[wow[y]]=max(res_len[wow[y]], tmp)
+prev=0
+start=0
 for i in range(n):
-    if first == 0:
-        if length[i] == max(length):
-            first = a[i]
-            print(first)
-            pre = a[i][:maxlength]
+    if start==0:
+        if res_len[i]==res:
+            start=temp[i]
+            print(start)
+            prev=temp[i][:res]
     else:
-        if length[i] == max(length) and a[i][:maxlength] == pre:
-            print(a[i])
+        if res_len[i]==res and temp[i][:res]==prev: 
+            print(temp[i])
             break
