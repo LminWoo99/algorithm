8c34f3a4789f5e557ddc7cfae68b69d69249be86
이민우 <mw310@naver.com>
2024-05-31 13:55:24 +0900

    [Gold V] Title: 탑, Time: 688 ms, Memory: 130844 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.py
+++ 백준/Gold/2493. 탑/탑.py
@@ -4,15 +4,18 @@ input=sys.stdin.readline
 n=int(input())
 top=list(map(int, input().split()))
 stack=[]
-res=[0]*(len(top))
+stack.append((0,0))
+res=[0]*n
 for i in range(n):
+    cur=top[i]
     while stack:
-        if stack[-1][1]>top[i]:
-            res[i]=stack[-1][0]+1
+        if stack[-1][1]<i and stack[-1][0]>cur:
+            res[i]=stack[-1][1]+1
             break
         else:
+            if not stack:
+                res[i]=0
+                break
             stack.pop()
-
-    stack.append((i, top[i]))
-        
+    stack.append((top[i],i))
 print(*res)
