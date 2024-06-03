ae024272a20cd43eb06c94af72c6773c662b8a36
이민우 <mw310@naver.com>
2024-06-03 13:22:47 +0900

    [Gold V] Title: 숫자고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -1,33 +1,32 @@
 import sys
-from itertools import combinations
 input=sys.stdin.readline
-ans = []
+
 def DFS(L):
-    if visit[L]==0:
+    global ans
+    if not visit[L]:
         visit[L]=1
-        for a in graph[L]:
+        for end in graph[L]:
             first.add(L)
-            second.add(a)
+            second.add(end)
             if first == second:
                 ans.extend(list(second))
-            DFS(a)
+            DFS(end)
     visit[L]=0
+            
 n=int(input())
 
 graph=[[] for _ in range(n+1)]
+ans=[]
+for i in range(n):
+    graph[i+1].append(int(input()))
 for i in range(1, n+1):
-    a=int(input())
-    graph[i].append(a)
-    
-for i in range(1, n+1):
-    visit=[0]*(n+1)
     first=set()
     second=set()
+    visit=[0]*(n+1)
     DFS(i)
+
 ans=list(set(ans))
 ans.sort()
 print(len(ans))
-for i in range(len(ans)):
-    print(ans[i])
-    
-    
+for i in ans:
+    print(i)
