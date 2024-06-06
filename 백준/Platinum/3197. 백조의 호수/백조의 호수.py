36e36ffe7f417d6b352e935f6f317ac1ca6ebd41
이민우 <mw310@naver.com>
2024-06-06 16:38:15 +0900

    [Platinum V] Title: 백조의 호수, Time: 1216 ms, Memory: 255232 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -1,76 +1,58 @@
-from sys import stdin
+import sys
+input=sys.stdin.readline
 from collections import deque
-input = stdin.readline
-
-dy = [-1, 0, 1, 0]
-dx = [0, -1, 0, 1]
-
-def find_swan(lake, visited, queue):
-    next_queue = deque()
-    while queue:
-        y, x  = queue.popleft()
-        if y == swan[1][0] and x == swan[1][1]:
-            return True, None
-        
-        for ii in range(4):
-            ny = y + dy[ii]
-            nx = x + dx[ii]
-            if not 0 <= ny < row or not 0 <= nx < column:
-                continue
-            if visited[ny][nx]:
-                continue
-            if lake[ny][nx] == 'X':
-                next_queue.append((ny, nx))
-            else:
-                queue.append((ny, nx))
-            visited[ny][nx] = True
-
-    return False, next_queue
-
-def melt_ice(water, lake):
-    next_water = deque()
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+def check_swan(dq):
+    next_dq=deque()
+    while dq:
+        x,y=dq.popleft()
+        if x==x2 and y==y2:
+            return 1, None
+        for i in range(4):
+            xx,yy=x+dx[i], y+dy[i]
+            if 0<=xx<r and 0<=yy<c and not visit[xx][yy] and lake[xx][yy]!='X':
+                visit[xx][yy]=1
+                dq.append((xx,yy))     
+            elif 0<=xx<r and 0<=yy<c and not visit[xx][yy] and lake[xx][yy]=='X':
+                visit[xx][yy]=1
+                next_dq.append((xx,yy))
+    return False, next_dq
+def BFS(water):
+    next_water=deque()
     while water:
-        y, x = water.popleft()
-        for ii in range(4):
-            ny = y + dy[ii]
-            nx = x + dx[ii]
-            if not 0 <= ny < row or not 0 <= nx < column:
-                continue
-            if lake[ny][nx] == 'X':
-                next_water.append((ny, nx))
-                lake[ny][nx] = '.'
-    
+        x,y=water.popleft()
+        for i in range(4):
+            xx,yy=x+dx[i], y+dy[i]
+            if 0<=xx<r and 0<=yy<c and lake[xx][yy]=='X':
+                lake[xx][yy]='.'
+                next_water.append((xx,yy))
     return next_water
+r,c=map(int, input().split())
+lake=[list(input().rstrip()) for _ in range(r)]
+swan=[]
+water=deque()
+for i in range(r):
+    for j in range(c):
+       if lake[i][j]=='L':
+           swan.append((i,j))
+           water.append((i,j))
+       elif lake[i][j]=='.':
+           water.append((i,j))
 
+x1,y1=swan[0][0], swan[0][1]           
+x2,y2=swan[1][0], swan[1][1]
+visit=[[0]*c for _ in range(r)]
+res=0
 
-row, column = map(int, input().split())
-lake = []
-swan = []
-water = deque()
-
-for rr in range(row):
-    current_lake_info = list(input().rstrip())
-    for cc, vv in enumerate(current_lake_info):
-        if vv == '.' or vv == 'L':
-            water.append((rr, cc))
-        if vv == 'L':
-            swan.append((rr, cc))
-    lake.append(current_lake_info)
-
-days = -1
-visited = [[False for _ in range(column)] for _ in range(row)]
-queue = deque()
-
-y, x = swan[0][0], swan[0][1]
-queue.append((y, x))
-visited[y][x] = True
+dq=deque()
+visit[x1][y1]=1
+dq.append((x1,y1))
 
 while True:
-    days += 1
-    found, next_queue = find_swan(lake, visited, queue)
+    found, dq=check_swan(dq)
     if found:
         break
-    water = melt_ice(water, lake)
-    queue = next_queue
-
-print(days)
+    res+=1
+    water=BFS(water)
+print(res)
