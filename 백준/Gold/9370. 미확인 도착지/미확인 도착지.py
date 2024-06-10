0109f31a74fa3822dea9d779965ea945ba7aa395
이민우 <mw310@naver.com>
2024-06-10 15:47:40 +0900

    [Gold II] Title: 미확인 도착지, Time: 280 ms, Memory: 45756 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -0,0 +1,43 @@
+import sys
+input = sys.stdin.readline
+from heapq import heappop, heappush
+def solution(start):
+    dist=[INF for _ in range(n+1)]
+    dist[start]=0
+    heap=[]
+    heappush(heap, [0, start])
+    while heap:
+        weight, node= heappop(heap)
+        if dist[node] < weight:
+            continue
+        for next, cost in graph[node]:
+            if dist[next]>weight+cost:
+                dist[next]=weight+cost
+                heappush(heap, [dist[next], next])
+    return dist
+T=int(input())
+INF=int(1e9)
+for _ in range(T):
+    n,m,t=map(int, input().split())
+    s,g,h=map(int, input().split())
+    graph=[[] for _ in range(n+1)]
+    for i in range(m):
+        a,b,d=map(int, input().split())
+        graph[a].append((b,d))
+        graph[b].append((a,d))
+    dest=[]
+    for i in range(t):
+        x=int(input())
+        dest.append(x)
+    distFromS=solution(s)
+    if distFromS[g]>distFromS[h]:
+        newStart=g
+    elif distFromS[g]<distFromS[h]:
+        newStart=h
+    distFromNewS=solution(newStart)
+    res=[]
+    for i in dest:
+        if distFromS[newStart]+distFromNewS[i]==distFromS[i]:
+            res.append(i)
+    res.sort()
+    print(*res)
