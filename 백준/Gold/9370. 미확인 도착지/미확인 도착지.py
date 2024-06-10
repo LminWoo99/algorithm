8df5acc6e17234ea2317bc8bf414fcac6c6f0bd1
이민우 <mw310@naver.com>
2024-06-10 15:53:09 +0900

    [Gold II] Title: 미확인 도착지, Time: 280 ms, Memory: 45740 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -1,43 +1,43 @@
-import sys
-input = sys.stdin.readline
-from heapq import heappop, heappush
+import sys, heapq
+input=sys.stdin.readline
 def solution(start):
-    dist=[INF for _ in range(n+1)]
-    dist[start]=0
-    heap=[]
-    heappush(heap, [0, start])
-    while heap:
-        weight, node= heappop(heap)
-        if dist[node] < weight:
+    hq=[]
+    distance=[int(1e9)]*(n+1)
+    heapq.heappush(hq, (0,start))
+    distance[start]=0
+    while hq:
+        dist, cur_node=heapq.heappop(hq)
+        if dist>distance[cur_node]:
             continue
-        for next, cost in graph[node]:
-            if dist[next]>weight+cost:
-                dist[next]=weight+cost
-                heappush(heap, [dist[next], next])
-    return dist
+        for next_node, next_dist in graph[cur_node]:
+            if distance[next_node]>next_dist+dist:
+                distance[next_node]=next_dist+dist
+                heapq.heappush(hq, (next_dist+dist, next_node))
+    return distance
 T=int(input())
-INF=int(1e9)
 for _ in range(T):
     n,m,t=map(int, input().split())
-    s,g,h=map(int, input().split())
+    s, g, h=map(int, input().split())
     graph=[[] for _ in range(n+1)]
-    for i in range(m):
+    for _ in range(m):
         a,b,d=map(int, input().split())
         graph[a].append((b,d))
         graph[b].append((a,d))
-    dest=[]
-    for i in range(t):
-        x=int(input())
-        dest.append(x)
-    distFromS=solution(s)
-    if distFromS[g]>distFromS[h]:
-        newStart=g
-    elif distFromS[g]<distFromS[h]:
-        newStart=h
-    distFromNewS=solution(newStart)
+    destination=[]
+    for _ in range(t):
+        destination.append(int(input()))
+    
+    startDistance=solution(s)
+    # 교차점중 거리가 큰게 기준이다. 최단거리를 구한것이고 거리가 큰게 g와 h사이에 도로로 지나갔다는 것
+    if startDistance[g]<startDistance[h]:
+        newS=h
+    else:
+        newS=g
+    secondDistance=solution(newS)
     res=[]
-    for i in dest:
-        if distFromS[newStart]+distFromNewS[i]==distFromS[i]:
-            res.append(i)
+    for dest in destination:
+        # 교차점을 지났을때의 목적지 거리+출발지에서의 교차점 거리 == 출발지에서 목적지까지의 최단 거리같아야함
+        if startDistance[newS]+secondDistance[dest]==startDistance[dest]:
+            res.append(dest)
     res.sort()
     print(*res)
