0045306e93af3631a939787bd29ccc42e95350a8
이민우 <mw310@naver.com>
2024-06-11 16:02:58 +0900

    [Gold IV] Title: 괄호, Time: 680 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/10422. 괄호/괄호.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+t=int(input())
+dp=[0]*5001
+dp[0]=1
+for n in range(2, 5001, 2):
+    for i in range(2, n+1, 2):
+        dp[n]+=dp[i-2]*dp[n-i]
+    dp[n]%=1000000007
+for _ in range(t):
+    L=int(input())
+    print(dp[L])
