ff5b1ab73918c307e8b6cc8d14ba4102fac05af3
이민우 <mw310@naver.com>
2024-06-16 14:19:58 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 100 ms, Memory: 43680 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.py
@@ -1,20 +1,15 @@
 import sys
 input=sys.stdin.readline
+
 n=int(input())
 a=list(map(int, input().split()))
 res=0
-visit=[0]*100001
-start, end=0,0
-while start<=end and end<n:
-    if not visit[a[end]]:
-        visit[a[end]]=1
-        end+=1
-        res+=end-start
-    else:
-        while visit[a[end]]:
-            visit[a[start]]=0
-            start+=1
-    
+check=set()
+start=0
+for end in range(n):
+    while a[end] in check:
+        check.remove(a[start])
+        start+=1
+    check.add(a[end])
+    res+=(end-start+1)
 print(res)
-    
-       
