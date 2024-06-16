fccebae60ba4147f8ef0ebb2baea20bb0eb0aeee
이민우 <mw310@naver.com>
2024-06-16 15:25:44 +0900

    [Silver I] Title: 트럭, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -1,31 +1,17 @@
 import sys
 input=sys.stdin.readline
-from collections import deque
 
 n,w,l=map(int, input().split())
 trucks=list(map(int, input().split()))
-dq=deque()
+
+bridge=[0]*w
 res=0
-idx=0
-cur_cnt=0
-cur_weight=0
-while True:
+while bridge:
     res+=1
-    if cur_cnt<w:
-        if cur_weight+trucks[idx]<=l:
-            dq.append([trucks[idx],w])
-            cur_weight+=trucks[idx]
-            cur_cnt+=1
-            idx+=1
-    if idx>=n:
-        if dq:
-            res+=dq[-1][1]
-        print(res)
-        break
-    if dq:
-        for truck in dq:
-            truck[1]-=1
-        if dq[0][1]<=0:
-            cur_weight-=dq[0][0]
-            dq.popleft()
-            cur_cnt-=1
+    bridge.pop(0)
+    if trucks:
+        if sum(bridge)+trucks[0]<=l:
+            bridge.append(trucks.pop(0))
+        else:
+            bridge.append(0)
+print(res)
