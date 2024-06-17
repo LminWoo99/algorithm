5e46d1231814904be6a6c48c4abf2cfe6e418f36
이민우 <mw310@naver.com>
2024-06-17 15:34:30 +0900

    [Gold V] Title: 빗물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,13 +1,12 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 h,w=map(int, input().split())
-block=list(map(int, input().split()))
-
+a=list(map(int, input().split()))
 res=0
 for i in range(1, w-1):
-   left=max(block[:i])
-   right=max(block[i+1:])
-   tmp=min(left, right)
-   if block[i]<tmp:
-       res+=tmp-block[i]
+    left=max(a[:i])
+    right=max(a[i+1:])
+    tmp=min(left, right)
+    if a[i]<tmp:
+        res+=(tmp-a[i])
 print(res)
