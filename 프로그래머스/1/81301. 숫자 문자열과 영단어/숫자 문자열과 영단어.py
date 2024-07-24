becc277e2074413f6665b0c7065c51b03ea03273
이민우 <mw310@naver.com>
2024-07-24 13:26:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,15 +1,5 @@
 def solution(s):
-    answer = ''
-    check={'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6,
-           'seven':7, 'eight':8, 'nine':9}
-    tmp=''
-    for i in s:
-        if i.isdigit():
-            answer+=i
-            continue
-        else:
-            tmp+=i
-            if tmp in check:
-                answer+=str(check[tmp])
-                tmp=''
-    return int(answer)
+    check={'zero':'0', 'one':'1', 'two':'2', 'three':'3', 'four':'4', 'five':'5', 'six':'6','seven':'7', 'eight':'8', 'nine':'9'}
+    for key, val in check.items():
+        s=s.replace(key, val)
+    return int(s)
