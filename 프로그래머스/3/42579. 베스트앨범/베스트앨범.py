20a6e738c9e9fe3e90ba4388d40b2ec1c855fd0f
이민우 <mw310@naver.com>
2024-07-26 21:50:07 +0900

    [level 3] Title: 베스트앨범, Time: 0.13 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+def solution(genres, plays):
+    answer = []
+    n=len(genres)
+    a=defaultdict(list)
+
+    for i in range(n):
+        a[genres[i]].append((plays[i],i))
+
+    tmp= sorted(a.values(), key=lambda x: sum(item[0] for item in x), reverse=True)
+    print(tmp)
+    for i in tmp:
+        if len(i)==1:
+            answer.append(i[0][1])
+        else:
+            i.sort(key=lambda x:(-x[0], x[1]))
+            print(i)
+            for j in range(2):
+                answer.append(i[j][1])
+        
+            
+    
+    	
+    
+    return answer
