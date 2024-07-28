7e76776044637c1a385d20ea41ee882aa16a2f1f
이민우 <mw310@naver.com>
2024-07-28 15:57:13 +0900

    [level 2] Title: 과제 진행하기, Time: 1.50 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -0,0 +1,30 @@
+
+def solution(plans):
+    answer = []
+    plans.sort(key=lambda x:x[1], reverse=True)
+    for i in plans:
+        i[1]=int(i[1][:2])*60+int(i[1][3:])
+    stop=[]
+    while len(plans)>=2:
+        start=plans[-1][1]
+        play=int(plans[-1][2])
+        next_start=plans[-2][1]
+
+        spare=next_start-(start+play)
+        if spare>=0:
+            answer.append(plans[-1][0])
+            plans.pop()
+            while spare>0 and stop:
+                if spare>=stop[-1][1]:
+                    spare-=stop[-1][1]
+                    answer.append(stop.pop()[0])
+                else:
+                    stop[-1][1]-=spare
+                    break
+        else:
+            stop.append([plans[-1][0], play-(next_start-start)])
+            plans.pop()
+    answer.append(plans.pop()[0])
+    answer.extend(name for name, play_time in reversed(stop))
+                
+    return answer
