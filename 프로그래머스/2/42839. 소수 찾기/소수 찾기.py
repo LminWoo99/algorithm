dead58143ee59baf321d632b541663b490df9850
이민우 <mw310@naver.com>
2024-08-05 21:56:37 +0900

    [level 2] Title: 소수 찾기, Time: 10.15 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,37 +1,48 @@
 from itertools import permutations
 import math
+
 def isPrime(target):
-    if target<=1:
+    if target <= 1:
         return False
-    if target==2:
+    if target == 2:
         return True
-    for i in range(2, int(math.sqrt(target))+1):
-        if target%i==0:
+    for i in range(2, int(math.sqrt(target)) + 1):
+        if target % i == 0:
             return False
     return True
-# def DFS(L, max_level):
-#     if L==max_level:
-#         return
+
+def solution(numbers):
+    answer = set()
+    cand = [num for num in numbers]  # numbers를 하나씩 나눠서 리스트
+    isPrime_list = set()
+
+    def DFS(tmp, used):
+        if tmp:
+            isPrime_list.add(int(tmp))
+        for i in range(len(cand)):
+            if not used[i]:
+                used[i] = True
+                DFS(tmp + cand[i], used)
+                used[i] = False
+
+    DFS("", [False] * len(cand))
     
-#     DFS(L+1)
-    
+    for num in isPrime_list:
+        if isPrime(num):
+            answer.add(num)
+
+    return len(answer)
+
+
 # def solution(numbers):
 #     ## 완탐 가능할듯
 #     answer=set()
 #     cand=[num for num in numbers]
-#     DFS(0, len(cand))
-#     ## 순열로 소수인지 파악
-#     return len(answer)
-def solution(numbers):
-    ## 완탐 가능할듯
-    answer=set()
-    cand=[num for num in numbers]
 
-    ## 순열로 소수인지 파악
-    for i in range(1, len(cand)+1):
-        for j in permutations(cand, i):
-            tmp=int(''.join(j))
-            if isPrime(tmp):
-                print(tmp)
-                answer.add(tmp)
-    return len(answer)
+#     ## 순열로 소수인지 파악
+#     for i in range(1, len(cand)+1):
+#         for j in permutations(cand, i):
+#             tmp=int(''.join(j))
+#             if isPrime(tmp):
+#                 answer.add(tmp)
+#     return len(answer)
