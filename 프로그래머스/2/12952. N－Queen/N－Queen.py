90a285559fa450eefa63b27a067cd815bf950578
이민우 <mw310@naver.com>
2024-08-06 15:21:10 +0900

    [level 2] Title: N-Queen, Time: 1288.12 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/2/12952. N－Queen/N－Queen.py
@@ -1,21 +1,18 @@
+
 def solution(n):
-	# 어태까지의 queen 위치 ls, 내가 두려는 위치 new
-    def check(ls, new):
-        for i in range(len(ls)):
-        	# 같은 열에 퀸을 둔 적이 있거나, 대각 위치에 둔 적이 있다면, return False
-            if new == ls[i] or (len(ls)-i) == abs(ls[i]-new):
-                return False
-        return True
-    def dfs(n, ls):
-    	# 끝 행까지 도달! return 1
-        if len(ls) == n:
+    def DFS(row, cols, diag1, diag2):
+        if row == n:
             return 1
-        # 끝 행이 아니라면, 다음 줄을 다시 탐색
-        cnt = 0
-        for i in range(n):
-            if check(ls, i):
-                cnt += dfs(n, ls+[i])
-        # 탐색 결과를 return
-        return cnt
-        
-    return dfs(n, [])
+        count = 0
+        for col in range(n):
+            if col not in cols and (row - col) not in diag1 and (row + col) not in diag2:
+                cols.add(col)
+                diag1.add(row - col)
+                diag2.add(row + col)
+                count += DFS(row + 1, cols, diag1, diag2)
+                cols.remove(col)
+                diag1.remove(row - col)
+                diag2.remove(row + col)
+        return count
+
+    return DFS(0, set(), set(), set())
