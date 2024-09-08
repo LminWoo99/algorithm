80d5ee67110509c05088af4d625f21d889d39222
이민우 <mw310@naver.com>
2024-09-08 15:07:26 +0900

    [level 2] Title: 괄호 변환, Time: 0.83 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,56 @@
+def divide(a):
+    if not a:
+        return ''
+    left=0
+    right=0
+    u,v='',''
+    for i in range(len(a)):
+        if a[i]==')':
+            left+=1
+        else:
+            right+=1
+        if left and right:
+            if left==right:
+                u=a[:i+1]
+                v=a[i+1:]
+                break
+    return u,v
+def check_correct(a):
+    stack=[]
+    if not a:
+        return False
+    for i in range(len(a)):
+        if stack:
+            if stack[-1]+a[i]=='()':
+                stack.pop()
+            else:
+                stack.append(a[i])
+        else:
+            stack.append(a[i])
+    if not stack:
+        return True
+    else:
+        return False
+def solution(p):
+    answer = ''
+    if not p:
+        return ''
+    else:
+        u,v=divide(p)
+        if check_correct(u):
+            return u+solution(v)
+        else:
+            answer='('
+            answer+=solution(v)
+            answer+=')'
+            for i in u[1: len(u)-1]:
+                if i=='(':
+                    answer+=')'
+                else:
+                    answer+='('
+            return answer
+                    
+            
+            
+            
+    return answer
