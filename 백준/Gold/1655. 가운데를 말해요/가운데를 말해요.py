76fab53a4880271c5c01d33accffe6c33c69df77
이민우 <mw310@naver.com>
2024-10-04 18:25:30 +0900

    [Gold II] Title: 가운데를 말해요, Time: 212 ms, Memory: 40988 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -2,19 +2,23 @@ import sys
 input=sys.stdin.readline
 import heapq
 
+## 짝수면 중간에 있는 두수중 작은수 왼쪽
 n=int(input())
+a=[]
 left=[]
+# 작은거 리스트에서 가장 큰 값 보다 크면 right, 작으면 left
 right=[]
-for i in range(n):
-    num=int(input())
+for _ in range(n):
+    x=int(input())
+    a.append(x)
     if len(left)==len(right):
-        heapq.heappush(left, -num)
+        heapq.heappush(left, -x)
     else:
-        heapq.heappush(right, num)
+        heapq.heappush(right, x)
     if right and right[0]<-left[0]:
-        leftRes=heapq.heappop(left)
-        rightRes=heapq.heappop(right)
-        
-        heapq.heappush(left, -rightRes)
-        heapq.heappush(right, -leftRes)
+        left_val=heapq.heappop(left)
+        right_val=heapq.heappop(right)
+
+        heapq.heappush(left, -right_val)
+        heapq.heappush(right,- left_val)
     print(-left[0])
