25139665131171bad65f655c48dbb7558a1c9365
이민우 <mw310@naver.com>
2024-10-04 21:20:28 +0900

    [Gold V] Title: 평범한 배낭, Time: 3876 ms, Memory: 227036 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -2,18 +2,19 @@ import sys
 input=sys.stdin.readline
 
 n,k=map(int, input().split())
-# 물건 리스트
-a=[[0,0]]
+
+dp=[[0]*(k+1) for _ in range(n+1)]
+
+#보석
+a=[]
 for _ in range(n):
     x,y=map(int, input().split())
     a.append([x,y])
-dp=[[0]*(k+1) for _ in range(n+1)]
-a.sort()
 for i in range(1, n+1):
+    cur_weight,cur_val=a[i-1][0],a[i-1][1]
     for j in range(1, k+1):
-        w,v=a[i]
-        if j<w:
+        if j<cur_weight:
             dp[i][j]=dp[i-1][j]
         else:
-            dp[i][j]=max(dp[i-1][j], dp[i-1][j-w]+v)
-print(max(map(max, dp)))
+            dp[i][j]=max(dp[i-1][j], dp[i-1][j-cur_weight]+cur_val)
+print(dp[-1][-1])
