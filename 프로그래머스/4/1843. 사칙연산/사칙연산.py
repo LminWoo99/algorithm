ff03ce12cc730eee9082e336fd81858fc26f0d7d
이민우 <mw310@naver.com>
2024-10-21 15:38:11 +0900

    [level 4] Title: 사칙연산, Time: 109.24 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.py
@@ -0,0 +1,29 @@
+def solution(arr):
+    M=dict()
+    m=dict()
+    nums = [int(x) for x in arr[::2]]
+    ops = [x for x in arr[1::2]]
+    for i in range(len(nums)):
+        M[(i,i)]=nums[i]
+        m[(i,i)]=nums[i]
+    for d in range(1, len(nums)):
+        for i in range(len(nums)):
+            j=i+d
+            if j>=len(nums):
+                continue
+            maxcandidates, mincandidates = [], []
+            for k in range(i+1, j+1):
+                if ops[k-1]=='-':
+                    MX=M[(i,k-1)]-m[(k, j)]
+                    mn=m[(i,k-1)]-M[(k, j)]
+                    maxcandidates.append(MX)
+                    mincandidates.append(mn)
+                else:
+                    MX=M[(i,k-1)]+M[(k, j)]
+                    mn=m[(i,k-1)]+m[(k, j)]
+                    maxcandidates.append(MX)
+                    mincandidates.append(mn)
+            M[(i,j)]=max(maxcandidates)
+            m[(i,j)]=min(mincandidates)
+    answer = M[(0,len(nums)-1)]
+    return answer
