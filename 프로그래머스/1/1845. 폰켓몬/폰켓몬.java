776776a848e1422fb4e581b8718f613f12b2767d
이민우 <mw310@naver.com>
2024-10-25 23:04:34 +0900

    [level 1] Title: 폰켓몬, Time: 3.76 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,18 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        int n=nums.length/2;
-        System.out.println(n);
-        Set<Integer> arr=new HashSet<>();
-        for(int num:nums){
-            arr.add(num);
-        }
-        if(arr.size()<n){
-            answer=arr.size(); 
-        }
-        else{
-            answer=n;
-        }
+     int answer=0;
+    Set<Integer> set=new HashSet<>();
+    for(int num:nums){
+        set.add(num);
+    }
+    int tmp=nums.length/2;
+    if (tmp<set.size()){
+        answer=tmp;
+    }
+    else{
+        answer=set.size();
+    }
         return answer;
     }
 }
