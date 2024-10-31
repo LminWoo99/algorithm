6e34a42fd6b38073422ee969ba39e0368f1315cc
이민우 <mw310@naver.com>
2024-10-31 15:12:49 +0900

    [level 2] Title: 소수 찾기, Time: 30.66 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,48 +1,46 @@
 import java.util.*;
 class Solution {
-    static Set<Integer> arr=new HashSet<>();
-    static int answer=0;
+    static Set<Integer> res=new HashSet<>();
+    static boolean[] visited;
     public int solution(String numbers) {
-        boolean[] visit=new boolean[numbers.length()];
-        DFS(numbers,visit, numbers.length(), "");
-        check();
-        return answer;
+        // ## DFS로 만들 수 있는 수를 모두 구하고 소수구하기
+        int n=numbers.length();
+        visited=new boolean[n];
+        DFS(numbers, "", n);
+        return isPrime();
 
     }
-    public void DFS(String numbers, boolean[] visit, int n, String cnt){
-        if (cnt!=""){
-            arr.add(Integer.parseInt(cnt));
+    public void DFS(String numbers, String number, int n){
+        if (number!=""){
+            res.add(Integer.valueOf(number));
         }
         for (int i=0; i<n; i++){
-            if (visit[i]==false){
-                visit[i]=true;
-                DFS(numbers, visit, n, cnt+numbers.charAt(i));
-                visit[i]=false;
-            }
-        }
+            if (visited[i]==false){
+                visited[i]=true;
 
+                DFS(numbers, number+numbers.charAt(i), n);
+                visited[i]=false;
+            }
+        }    
     }
-    
-    public void check(){
+    public int isPrime(){
+        int cnt=0;
 
-        for (int target : arr){
+        for (int target : res){
+            if (target<2){
+                continue;
+            }
             boolean flag=true;
-            if (target==2){
-                answer+=1;
-                continue;
-            }
-            if (target<=1){
-                continue;
-            }
-            for (int i=2; i<(int) Math.sqrt(target)+1; i++){
+            for (int i=2; i<(int)Math.sqrt(target)+1; i++){
                 if (target%i==0){
                     flag=false;
                     break;
                 }
             }
-            if (flag){
-                answer+=1;
+            if (flag==true){
+                cnt+=1;
             }
         }
+        return cnt;
     }
 }
