fc2bf34262427d411c39018f005a8a9223c09378
이민우 <mw310@naver.com>
2024-11-01 12:12:19 +0900

    [level 3] Title: 숫자 게임, Time: 122.06 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int answer = 0;
+        PriorityQueue<Integer> pqA=  new PriorityQueue<>();
+        PriorityQueue<Integer> pqB=  new PriorityQueue<>();
+        for (int i=0; i<A.length; i++){
+            pqA.add(-A[i]);
+            pqB.add(-B[i]);
+        }
+        // 2457 
+        //    1368
+        // 2459
+        //     1368
+        while (!pqA.isEmpty()){
+            int cntA=-pqA.poll();
+            int cntB=-pqB.poll();
+            if (cntA<cntB){
+                answer+=1;
+            }
+            else{
+                pqB.add(-cntB);
+            }
+            
+        }
+        return answer;
+    }
+}
