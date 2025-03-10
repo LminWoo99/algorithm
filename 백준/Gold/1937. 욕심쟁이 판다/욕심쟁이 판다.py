76ba7a7bb0e360b5f224737928646d51bb2fc2c4
이민우 <mw310@naver.com>
2025-03-10 13:30:30 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 720 ms, Memory: 52184 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.py
@@ -1,27 +1,24 @@
-dx=[-1,0,1,0]
-dy=[0,1,0,-1]
-
 import sys
 input=sys.stdin.readline
 sys.setrecursionlimit(10**6)
+res=0
 def DFS(x,y):
     if dp[x][y]:
         return dp[x][y]
     dp[x][y]=1
     for i in range(4):
-        xx,yy=x+dx[i], y+dy[i]
-        if 0<=xx<n and 0<=yy<n and forest[x][y]<forest[xx][yy]:
-            dp[x][y]=max(dp[x][y], DFS(xx,yy)+1)
+        xx=x+dx[i]
+        yy=y+dy[i]
+        if 0<=xx<n and 0<=yy<n and board[x][y]<board[xx][yy]:
+            dp[x][y]=max(DFS(xx,yy)+1, dp[x][y])
     return dp[x][y]
-        
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
+
 n=int(input())
-forest=[list(map(int, input().split())) for _ in range(n)]
-
+board=[list(map(int, input().split())) for _ in range(n)]
 dp=[[0]*n for _ in range(n)]
-
-answer=0
-
 for i in range(n):
     for j in range(n):
-        answer=max(answer, DFS(i,j))
-print(answer)
+        res=max(res, DFS(i,j))
+print(res)
