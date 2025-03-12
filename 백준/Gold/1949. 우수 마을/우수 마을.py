e7c0ad3dcdd3fdd0a77696715432939d680f6fa7
이민우 <mw310@naver.com>
2025-03-12 13:45:00 +0900

    [Gold II] Title: 우수 마을, Time: 60 ms, Memory: 37048 KB -BaekjoonHub

--- 백준/Gold/1949. 우수 마을/우수 마을.py
+++ 백준/Gold/1949. 우수 마을/우수 마을.py
@@ -1,23 +1,25 @@
 import sys
 input=sys.stdin.readline
 sys.setrecursionlimit(10**7)
-
-def DFS(start):
-    visited[start]=1
-    for next_node in towns[start]:
-        if not visited[next_node]:
-            DFS(next_node)
-            dp[start][1]+=dp[next_node][0]
-            dp[start][0]+=max(dp[next_node][0], dp[next_node][1])
+def DFS(cur_node):
+    visited[cur_node]=1
+    for next in trees[cur_node]:
+        if not visited[next]:
+            DFS(next)
+            dp[cur_node][1]+=dp[next][0]
+            dp[cur_node][0]+=max(dp[next][0], dp[next][1])
+            
 n=int(input())
-town_peoples=[0]+list(map(int, input().split()))
-dp=[[0, town_peoples[i]]*2 for i in range(n+1)]
+
+towns=[0] + list(map(int, input().split()))
 visited=[0]*(n+1)
-towns=[[]*(n+1) for _ in range(n+1)]
+trees=[[]*(n+1) for _ in range(n+1)]
+dp=[[0, towns[i]] for i in range(n+1)]
 
 for _ in range(n-1):
-    a,b=map(int, input().split())
-    towns[a].append(b)
-    towns[b].append(a)
+    x,y=map(int, input().split())
+    trees[x].append(y)
+    trees[y].append(x)
+
 DFS(1)
 print(max(dp[1][0],dp[1][1]))
