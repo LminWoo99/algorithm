dafb01a0544c495c32b2b81d47ab21ac7ee4677b
이민우 <mw310@naver.com>
2025-03-17 14:31:13 +0900

    [Gold V] Title: 암호코드, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.py
+++ 백준/Gold/2011. 암호코드/암호코드.py
@@ -1,16 +1,16 @@
 import sys
 input=sys.stdin.readline
-
 a=input().rstrip()
+
 dp=[0]*(len(a)+1)
-if a[0]=='0':
-    print(0)
-else:
-    dp[0]=dp[1]=1
-    for i in range(2, len(a)+1):
-        if int(a[i-1])>0:
-            dp[i]+=dp[i-1]
-        tmp=int(a[i-2]+a[i-1])
-        if tmp>=10 and tmp<=26:
+dp[0]=1
+for i in range(1, len(a)+1):
+    if i>=2 and not int(a[i-2:i]):
+        dp[-1]=0
+        break
+    if int(a[i-1]):
+        dp[i]+=dp[i-1]
+    if i>=2:
+        if a[i-2]!='0' and int(a[i-2:i])<27:
             dp[i]+=dp[i-2]
-    print(dp[-1]%1000000)
+print(dp[-1]%1000000)
