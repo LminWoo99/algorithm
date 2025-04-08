5034cff3d87c37a2be3c029aaaca91ea86b77881
이민우 <mw310@naver.com>
2025-04-08 15:30:05 +0900

    Time: 0 ms (100%), Space: 13.1 MB (61.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,8 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        res=[0,0]
-        tmp=[[0]*2 for _ in range(len(nums))]
+        nums_dict=dict()
         for idx, val in enumerate(nums):
-            tmp[idx][0]=val
-            tmp[idx][1]=idx
-        tmp.sort()
-        for i in range(len(tmp)-1):
-            start,end=i, len(tmp)-1
-            flag=False
-            while start<end:
-                mid=tmp[start][0]+tmp[end][0]
-                if mid==target:
-                    flag=True
-                    res[0]=tmp[start][1]
-                    res[1]=tmp[end][1]
-                    break
-                if mid>target:
-                    end-=1
-                else:
-                    start+=1
-            if flag:
-                break
-        return res
+            if (target-val) in nums_dict:
+                return [nums_dict[target-val], idx]
+            nums_dict[val]=idx
+        return None
